61cacadc854cd8f6c10d10a0bd53774ac488ff45
jeremieoyabun <jeremie@oyabun.io>
2026-02-13 14:23:52 +0100

    Fix duplicate footer + UI cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Nunito, DM_Sans, DM_Mono } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
-import { Footer } from "@/src/components/Footer";
 import "./globals.css";
 
 const nunito = Nunito({ subsets: ["latin"], variable: "--font-display", weight: ["700", "800", "900"] });
